9e05d5c8a32e7c2b7fa097ba1f504f6ba749f53e
wellington santos <50835319+well777@users.noreply.github.com>
2023-07-28 23:08:56 -0300

    Create loopfor.dart

---
+++ loopfor.dart
@@ -0,0 +1,18 @@
+void main() {
+
+	
+	for (int i = 1; i <= 10; i++) {
+
+		if ( i % 2 == 0) {
+			print(i);
+		}
+	}
+
+
+	// for ..in loop
+	List planetList = ["Mercury", "Venus", "Earth", "Mars"];
+
+	for (String planet in planetList) {
+		print(planet);
+	}
+}
