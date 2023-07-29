10addc8c5cce487a93d0bd9dd33eddda17b440f2
wellington santos <50835319+well777@users.noreply.github.com>
2023-07-28 23:06:37 -0300

    Create switch_and_case.dart

---
+++ switch_and_case.dart
@@ -0,0 +1,19 @@
+void main() {
+	String grade = 'A';
+	switch (grade) {
+		case 'A':
+			print("Excellent grade of A");
+			break;
+		case 'B':
+			print("Very Good !");
+			break;
+		case 'C':
+			print("Good enough. But work hard");
+			break;
+		case 'F':
+			print("You have failed");
+			break;
+		default:
+			print("Invalid Grade");
+	}
+}
